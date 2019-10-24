b3e3012bcd10baa523efd22e39002f7cb5a1b7ec
Claud Watari <claudwatari95@gmail.com>
2019-10-25 01:36:43 +0300

    #Add Prop Interface

--- pluralsight-projects-Angular-AlbumStoreProductPage-27449b2/src/app/product.service.ts
+++ pluralsight-projects-Angular-AlbumStoreProductPage-27449b2/src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable'
 
 
 @Injectable()
@@ -15,9 +17,9 @@ export class ProductService {
 
 	private _albumUrl = '../assets/album.json'
 
-	getAlbum(id: number) {
+	getAlbum(id: number): Observable<Album> {
 		return this._http.get(this._albumUrl)
-		.map(response => response.json())
+			.map(response => <Album> (response.json()))
 	}
 
 }
